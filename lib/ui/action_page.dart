801798f93e16dee4e90df47f758772f6bc265b3a
Anis Memom <anismemom@Aniss-MacBook-Pro.local>
2019-08-08 01:13:58 +0500

    video bg in progress

--- lib/ui/action_page.dart
+++ lib/ui/action_page.dart
@@ -23,10 +23,9 @@ class _ActionPageState extends State<ActionPage> {
         // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
         setState(() {});
       });
-      _controller.play();
-      _controller.setLooping(true);
-      _controller.setVolume(0.0);
-
+    _controller.play();
+    _controller.setLooping(true);
+    _controller.setVolume(0.0);
   }
 
   DecorationImage _buildBackgroundImage() {
@@ -46,107 +45,116 @@ class _ActionPageState extends State<ActionPage> {
     double mainFontSize = width * 0.07;
 
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: _buildBackgroundImage(),
-        ),
-        child: Column(
-          children: [
-            Expanded(
-              child: Container(
-                alignment: Alignment(0.9, -0.9),
-                child: IconButton(
-                  icon: Icon(
-                    Icons.menu,
-                    color: Color.fromRGBO(0, 0, 0, 1),
-                    size: 60,
-                  ),
-                  onPressed: () {
-                    Navigator.pushReplacementNamed(context, '/unlock');
-                  },
-                ),
-              ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: _buildBackgroundImage(),
             ),
-            SingleChildScrollView(
-              child: Container(
-                margin: EdgeInsets.only(bottom: 230.0),
-                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Container(
-                      margin: EdgeInsets.only(bottom: 20.0),
-                      child: Text(
-                        'TAP TO START',
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    alignment: Alignment(0.9, -0.9),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.menu,
+                        color: Color.fromRGBO(0, 0, 0, 1),
+                        size: 60,
+                      ),
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, '/unlock');
+                      },
+                    ),
+                  ),
+                ),
+                SingleChildScrollView(
+                  child: Container(
+                    margin: EdgeInsets.only(bottom: 230.0),
+                    padding:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.only(bottom: 20.0),
+                          child: Text(
+                            'TAP TO START',
+                            style: TextStyle(
+                                fontFamily: 'Proxima',
+                                fontWeight: FontWeight.w700,
+                                fontSize: mainFontSize,
+                                color: Colors.white),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            StoriActionButton(
+                              text: 'PHOTO',
+                              onPressed: () {
+                                Navigator.pushReplacementNamed(
+                                    context, '/preview');
+                              },
+                            ),
+                            StoriActionButton(
+                              text: 'BOOMERANG',
+                              onPressed: () {
+                                Navigator.pushReplacementNamed(
+                                    context, '/preview');
+                              },
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            StoriActionButton(
+                              text: 'SHOUTOUT',
+                              onPressed: () {
+                                Navigator.pushReplacementNamed(
+                                    context, '/preview');
+                              },
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                Container(
+                  // height: 90.0,
+                  child: Column(
+                    children: [
+                      Text(
+                        "by using this application you agree to our Privacy Policy and Terms of use",
                         style: TextStyle(
-                            fontFamily: 'Proxima',
-                            fontWeight: FontWeight.w700,
-                            fontSize: mainFontSize,
+                            // fontWeight: FontWeight.w200,
+                            fontSize: 16,
                             color: Colors.white),
                       ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        StoriActionButton(
-                          text: 'PHOTO',
-                          onPressed: () {
-                            Navigator.pushReplacementNamed(context, '/preview');
-                          },
+                      SizedBox(height: 10.0),
+                      Container(
+                        width: double.infinity,
+                        height: 50.0,
+                        padding: EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(0, 0, 0, 0.5),
                         ),
-                        StoriActionButton(
-                          text: 'BOOMERANG',
-                          onPressed: () {
-                            Navigator.pushReplacementNamed(context, '/preview');
-                          },
+                        child: Image.asset(
+                          "assets/images/powerby-halocam@2x.png",
+                          fit: BoxFit.contain,
                         ),
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        StoriActionButton(
-                          text: 'SHOUTOUT',
-                          onPressed: () {
-                            Navigator.pushReplacementNamed(context, '/preview');
-                          },
-                        ),
-                      ],
-                    )
-                  ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+              ],
             ),
-            Container(
-              // height: 90.0,
-              child: Column(
-                children: [
-                  Text(
-                    "by using this application you agree to our Privacy Policy and Terms of use",
-                    style: TextStyle(
-                        // fontWeight: FontWeight.w200,
-                        fontSize: 16,
-                        color: Colors.white),
-                  ),
-                  SizedBox(height: 10.0),
-                  Container(
-                    width: double.infinity,
-                    height: 50.0,
-                    padding: EdgeInsets.all(8.0),
-                    decoration: BoxDecoration(
-                      color: Color.fromRGBO(0, 0, 0, 0.5),
-                    ),
-                    child: Image.asset(
-                      "assets/images/powerby-halocam@2x.png",
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
